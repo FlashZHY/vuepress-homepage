43ea966d25ac19e2461dc86b030a92519cd0fb7f
FlashZHY <67984926+FlashZHY@users.noreply.github.com>
2020-11-01 21:52:00 -0500

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,6 @@ module.exports = {
       { text: "Home", link: "/" },
       { text: "About", link: "/about/" },
       { text: "Projects", link: "/projects/" },
-      { text: "Guide", link: "/guide/" },
       { text: "GitHub", link: "https://github.com/FlashZHY" }
     ],
     sidebar: {
